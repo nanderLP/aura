59fa60b5873d4d79928d5796a63b2a1e94503fea
nanderLP <nander@nander.dev>
2022-05-27 11:29:35 +0200

    cleanup

--- web/src/lib/firebase.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-};
-
-initializeApp(firebaseConfig);
